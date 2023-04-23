a45ff5b54a6e0c1568fb5ed2e4d14077a7910944
DishaVai <disha.v@outlook.com>
2023-04-22 17:39:50 -0700

    Should now alternate between adding an x and an o

---
+++ games_page/tic-tac-toe.js
@@ -0,0 +1,22 @@
+var turn = 1;
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    const el = document.getElementById("tic-tac-toe");
+    if (el) {
+      el.addEventListener('click', (ev) => {
+        const [x, y] = [
+          ev.target.cellIndex, 
+          ev.target.parentElement.rowIndex
+        ];
+        if (x === undefined || y === undefined) {
+          return;
+        }
+        if(turn % 2 == 0) {
+            ev.target.innerHTML = "O";
+        }else {
+            ev.target.innerHTML = "X";
+        }
+        turn++;
+      });
+    }
+});
